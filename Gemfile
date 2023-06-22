b74f206b9096d801996844493ab8d322905b5714
rbhatt199924 <rbhatt199924@gmail.com>
2023-06-22 17:16:23 +0530

    add users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
+gem 'dotenv-rails', '~> 2.1', '>= 2.1.1'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
@@ -70,3 +71,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "devise", "~> 4.9"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,19 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV["HOST"], port: 3000 }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["EMAIL"],
+    password: ENV["PASSWORD"],
+    domain: ENV["HOST"],
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -37,7 +50,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
